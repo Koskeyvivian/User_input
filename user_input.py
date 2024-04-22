fa52d282e00f7b0c5a92396680ef64e0b9f7e40e
Koskeyvivian <vcherono39@gmail.com>
2024-04-22 22:31:51 +0300

    Changes name

--- user_input.py
+++ user_input.py
@@ -2,5 +2,5 @@ name = input("What is your name? ")
 age = input("How old are you? ")
 location = input("Where are you located? ")
 
-message = f"Hello {"Vivian"}! You are {19} years old and located in {"Germany"}."
+message = f"Hello {"Fatima"}! You are {19} years old and located in {"Germany"}."
 print(message)
