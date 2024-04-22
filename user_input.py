264e39946ccdd98dd5f0087b53174a02b9eaec60
Koskeyvivian <vcherono39@gmail.com>
2024-04-22 18:41:59 +0300

    My first python commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+message = f"Hello {"Vivian"}! You are {19} years old and located in {"Germany"}."
+print(message)
